03738a482043e5f4f8d5e4328526b4f9f0b080e5
anuroopvijayan <anuroop.vijayan@ext.deltacapita.com>
2025-10-21 14:12:30 +0530

    deploy script

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,15 +25,5 @@ module.exports = {
       },
       __key: 'images',
     },
-    {
-      resolve: 'gatsby-source-strapi',
-      options: {
-        apiURL: 'http://localhost:1337', // or your Strapi Cloud URL
-        accessToken:
-          '01dfcaf4780e4f5e3e30b25984f6515e368d6b24156e71f18b7863fb36ded788ec10cb23d631af82645b025c64f415fc1225b1e5925979d6c4795c63d15d9c30945913304fabf0505b75a31ea08a4099460b19416b39be63814b6c557ca919748fc1fd6beb4fa34523382cb2f3f5e76dcb8c8428cdcd6d6ad008726131f27719', // optional, if you have a public or private token
-        collectionTypes: ['venue'], // your content type(s)
-        queryLimit: 1000,
-      },
-    },
   ],
 };
